9d24d179c53a125295a8e69984e9477100492fb7
pewguin <jonas.brastad@gmail.com>
2025-08-22 17:41:22 -0700

    transform

---
+++ src/rendering/transform.rs
@@ -0,0 +1,19 @@
+use glam::{Mat4, Quat, Vec3};
+
+pub struct Transform {
+    pub scale: Vec3,
+    pub rot: Quat,
+    pub pos: Vec3,
+}
+
+impl Transform {
+    pub fn get_transform(&self) -> Mat4 {
+        Mat4::from_scale_rotation_translation(self.scale, self.rot, self.pos)
+    }
+}
+
+impl Default for Transform {
+    fn default() -> Self {
+        Self { scale: Vec3::ONE, rot: Quat::IDENTITY, pos: Vec3::ZERO }
+    }
+}
